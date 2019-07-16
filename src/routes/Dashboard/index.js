4e6b893b15619593fda08524454d2ae06d836f53
brnaldomesi <brnaldomesi1@gmail.com>
2019-07-16 21:40:02 +0900

    Add simple menu to dashboard page

--- src/routes/Dashboard/index.js
+++ src/routes/Dashboard/index.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
 import { compose } from 'redux'
+import { Link } from 'react-router-dom'
 import { withStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import IconCast from '@material-ui/icons/Cast'
+import IconNetwork from '@material-ui/icons/Share'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
 import PropTypes from 'prop-types'
 
 import { userIsAuthenticatedRedir } from 'hocs/withAuth'
@@ -19,6 +27,24 @@ class Dashboard extends Component {
     return (
       <div className={classes.root}>
         <Typography variant="h4">Welcome to Cadence Showtime!</Typography>
+        <Grid container>
+          <Grid item sm={6}>
+            <List component="nav" aria-label="Navigation" className={classes.list}>
+              <ListItem button component={Link} to="/podcasts">
+                <ListItemIcon>
+                  <IconCast />
+                </ListItemIcon>
+                <ListItemText primary="View Podcasts" />
+              </ListItem>
+              <ListItem button component={Link} to="/">
+                <ListItemIcon>
+                  <IconNetwork />
+                </ListItemIcon>
+                <ListItemText primary="View Networks (Coming Soon)" />
+              </ListItem>
+            </List>
+          </Grid>
+        </Grid>
       </div>
     )
   }
