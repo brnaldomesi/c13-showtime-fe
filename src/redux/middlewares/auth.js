ba8b747d9beda361dbd216fbe2f4c87669c25c47
brnaldomesi <brnaldomesi1@gmail.com>
2019-07-24 22:18:54 +0900

    Fix auth auto logout not working

--- src/redux/middlewares/auth.js
+++ src/redux/middlewares/auth.js
@@ -1,9 +1,11 @@
+import get from 'lodash/get'
+
 import { AUTH_LOGOUT } from 'redux/modules/auth'
 import { REQUEST_REJECTED } from 'redux/modules/api'
 
 const authMiddleware = store => next => action => {
   if (action.type === REQUEST_REJECTED) {
-    const { status } = action.payload || {}
+    const status = get(action, 'payload.data.status')
     if (status === 401) {
       store.dispatch({
         type: AUTH_LOGOUT
