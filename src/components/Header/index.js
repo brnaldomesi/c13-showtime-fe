4da486964048f6ce4a8ed7281315b14a2a43b44f
brnaldomesi <brnaldomesi1@gmail.com>
2019-07-30 01:52:46 +0900

    Add searchbox UI, podcasts & networks link into the header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,26 +1,46 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import AppBar from '@material-ui/core/AppBar'
+import Button from '@material-ui/core/Button'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
+import InputBase from '@material-ui/core/InputBase'
 import MenuIcon from '@material-ui/icons/Menu'
+import SearchIcon from '@material-ui/icons/Search'
+import { Link } from 'react-router-dom'
+import { withStyles } from '@material-ui/core'
+
 import styles from './styles'
-import { withStyles } from '@material-ui/core';
 
 export const Header = ({ classes, toggleSidebar }) => (
   <AppBar position="fixed" className={classes.root}>
     <Toolbar disableGutters>
-      <IconButton
-        color="inherit"
-        onClick={() => toggleSidebar(true)}
-        className={classes.menuButton}
-      >
+      <IconButton color="inherit" onClick={() => toggleSidebar(true)} className={classes.menuButton}>
         <MenuIcon />
       </IconButton>
       <Typography variant="h6" color="inherit" noWrap>
         Cadence13 Showtime
       </Typography>
+      <div className={classes.search}>
+        <div className={classes.searchIcon}>
+          <SearchIcon />
+        </div>
+        <InputBase
+          placeholder="Find a podcast..."
+          classes={{
+            root: classes.inputRoot,
+            input: classes.inputInput
+          }}
+          inputProps={{ 'aria-label': 'search' }}
+        />
+      </div>
+      <Button component={Link} to="/podcasts" color="inherit">
+        Podcasts
+      </Button>
+      <Button component={Link} to="/networks" color="inherit">
+        Networks
+      </Button>
     </Toolbar>
   </AppBar>
 )
